4a7f758234e8e1f3eaa8a59d4a5d792d1a8359dd
Andrey Vazhenov <andrejvazenov@gmail.com>
2022-03-10 00:48:35 +0700

    Добавил метод делегата и accessories typre

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -30,6 +30,7 @@ class ToDoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
         cell.textLabel?.text = itemArray[indexPath.row]
 
     
@@ -37,9 +38,23 @@ class ToDoListViewController: UITableViewController {
         return cell
     }
 
-
+    
+    // MARK: - Table view Delegate Methods
+    
  
-
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       
+        //print(itemArray[indexPath.row])
+        
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
     
 
    
