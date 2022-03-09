3d1e112e83c54b69bb80782d3201c08b75b0cb8c
Andrey Vazhenov <andrejvazenov@gmail.com>
2022-03-10 00:16:25 +0700

    Настроил табличное представление и заполнил его тестовым массивом

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
 
+    
     let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
     
     override func viewDidLoad() {
@@ -18,6 +19,31 @@ class ToDoListViewController: UITableViewController {
     
     }
 
+    // MARK: - Table view data source
+
   
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       
+        return itemArray.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+
+    
+
+        return cell
+    }
+
+
+ 
+
+    
+
+   
+
+    
 
 }
