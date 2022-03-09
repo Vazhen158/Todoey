81ecb0a03cac092cc41f31759e0a0fdbcded4f76
Andrey Vazhenov <andrejvazenov@gmail.com>
2022-03-09 23:42:12 +0700

    Добавил Navigation Controller и ToDoListViewController

---
+++ Todoey/ToDoListViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ToDoListViewController.swift
+//  Todoey
+//
+//  Created by Андрей Важенов on 09.03.2022.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+
+    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+
+  
+
+}
