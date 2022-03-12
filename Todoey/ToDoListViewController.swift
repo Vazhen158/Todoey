c1a9b8809dd6fd98154de3ed0f298582fdfd4f56
Andrey Vazhenov <andrejvazenov@gmail.com>
2022-03-12 23:16:16 +0700

    Добавил возможность сохранять данные с помощью UserDefaults

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -13,10 +13,13 @@ class ToDoListViewController: UITableViewController {
     
     var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
     
+    let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        if let items = defaults.array(forKey: "ToDoListArray") as? [String] {
+            itemArray = items
+        }
         
     }
     
@@ -66,6 +69,8 @@ class ToDoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             
             self.itemArray.append(textField.text!)
+            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
+            
             self.tableView.reloadData()
         }
         alert.addTextField { (alertTextField) in
